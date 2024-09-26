f3659afe671d33f7edb2113fa9f3f056f1dedfef
DESKTOP-TTQOC8H\Mostov <vladimircev327@gmail.com>
2024-09-26 18:57:00 +0400

    Добавление задачи 5

--- src/main/java/org/knit/lab2/Task4.java
+++ src/main/java/org/knit/lab2/Task4.java
@@ -2,15 +2,13 @@ package org.knit.lab2;
 
 public class Task4 {
     public static void main(String[] args) {
-        Container container = new Container(1000);
+        Container container = new Container(1000);  // Создаем контейнер с объемом 1000
 
-        Shape sphere = new Sphere(5);
-        Shape cube = new Cube(3);
-        Shape cylinder = new Cylinder(2, 4);
+        Shape sphere = new Sphere(5);   // Создаем сферу с радиусом 5
+        Shape cube = new Cube(3);       // Создаем куб со стороной 3
 
-        container.add(sphere);
-        container.add(cube);
-        container.add(cylinder);
+        container.add(sphere);  // Проверяем, можно ли добавить сферу
+        container.add(cube);    // Проверяем, можно ли добавить куб
     }
 }
 
